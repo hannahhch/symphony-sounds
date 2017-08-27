cc1a30cae4a9f263d4a7aaf58731cf5f6f1e48dc
Hannah Hudson <hannah.hch@gmail.com>
2017-08-27 10:57:35 -0400

    percussion sounds

--- main/percussion.js
+++ main/percussion.js
@@ -12,3 +12,31 @@ function playDjembe() {
     let snd = new Audio("audio/djembe.mp3");
     snd.play();
 };
+
+let cymSound = document.querySelector('#cymbalsClick');
+cymSound.addEventListener('click', playCym);
+function playCym(){
+  let snd = new Audio("audio/cymbals.mp3");
+  snd.play();
+};
+
+let snareSound = document.querySelector('#snareClick');
+snareSound.addEventListener('click', playSnare);
+function playSnare(){
+  let snd = new Audio("audio/snare.mp3");
+  snd.play();
+};
+
+let tamSound = document.querySelector('#tamClick');
+tamSound.addEventListener('click', playTam);
+function playTam(){
+  let snd = new Audio("audio/tambourine.mp3");
+  snd.play();
+};
+
+let triSound = document.querySelector('#triClick');
+triSound.addEventListener('click', playTri);
+function playTri(){
+  let snd = new Audio("audio/triangle.mp3");
+  snd.play();
+};
